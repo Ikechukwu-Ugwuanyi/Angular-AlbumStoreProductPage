b7122fc0efb865f3305cb62c3988940905c9c0ed
Ikechukwu-Ugwuanyi <83365835+Ikechukwu-Ugwuanyi@users.noreply.github.com>
2021-11-26 19:18:37 +0100

    Part 6 code of the Album Store

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -3,10 +3,12 @@ import { Http, Response } from '@angular/http';
 import { Observable } from 'rxjs';
 import 'rxjs/add/operator/map';
 import { Album } from './album';
+import { Product } from './product';
 
 
 @Injectable()
 export class ProductService {
+  _productsUrl = '../assets/products.json';
 
   constructor(private _http: Http) { }
   private _albumUrl = '../assets/album.json';
@@ -14,6 +16,11 @@ export class ProductService {
   getAlbum(id: number): Observable<Album> {
     return this._http.get(this._albumUrl).map((response) =>
      <Album>response.json());
+  }
+
+  getProducts() {
+    return this._http.get(_productsUrl).map(response => 
+      response.json());
   }
 
 }
